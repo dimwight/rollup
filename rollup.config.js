682de7c2cd84d71dd6279726ceae9dcc557f656c
dimwight <dimwight@ntlworld.com>
2017-07-30 17:10:33 +0100

    Ready for React?

--- rollup.config.js
+++ rollup.config.js
@@ -3,13 +3,14 @@ import commonjs from 'rollup-plugin-commonjs';
 import sourcemaps from 'rollup-plugin-sourcemaps';
 
 export default {
-	entry: 'src/main.js',
-	dest: 'public/bundle.js',
-    format: 'cjs',
-    sourceMap: true,
-    plugins: [
-		resolve(),
-		commonjs(),
-        sourcemaps()
-	]
+  entry: 'src/main.js',
+  dest: 'public/bundle.js',
+  format: 'cjs',
+  sourceMap: true,
+  plugins: [
+    resolve(),
+    commonjs(),
+    
+    sourcemaps()
+  ]
 };
